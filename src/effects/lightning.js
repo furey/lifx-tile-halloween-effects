86959d2176f2fb2be9ada8674977f9a5431d93df
James Furey <jfurey@gmail.com>
2019-10-14 16:58:05 +1100

    Fleshed out lighting effect logic.

--- src/effects/lightning.js
+++ src/effects/lightning.js
@@ -15,6 +15,7 @@ module.exports = class {
     this.device = device
     this.tiles = tiles
     this.bounds = bounds
+    this.bolt = undefined
     this.canvas = undefined
     this.colors = undefined
   }
@@ -28,42 +29,93 @@ module.exports = class {
   }
 
   setInitialState() {
-    this.canvas = this.createLightning()
+    this.bolt = this.createBolt()
   }
 
-  createLightning() {
-    const canvas = [...Array(this.bounds.height)].map(() =>
-      [...Array(this.bounds.width)]
-    )
-    let x = Math.floor(Math.random() * this.bounds.width)
-    for (let y = 0; y < this.bounds.height; y++) {
-      x = x - 1 + Math.floor(Math.random() * 3)
-      x = Math.max(0, Math.min(this.bounds.width - 1, x))
-      canvas[y][x] = 'w'
+  createBolt() {
+    return {
+      origin: Math.floor(Math.random() * this.bounds.width),
+      flashCount: 0,
+      totalFlashes: 1 + Math.floor(Math.random() * 3),
+      delay: 0,
+      state: 'flash'
     }
-    return canvas
   }
 
   async step() {
-    this.stepCanvas()
-    this.renderCanvas()
-    await this.updateTiles()
+    await this.stepBolt()
   }
 
-  stepCanvas() {
-    this.canvas = this.createLightning()
+  async stepBolt() {
+    this.bolt.delay -= this.getStepMilliseconds()
+    if (this.bolt.delay > 0) return
+    switch (this.bolt.state) {
+      case 'flash':
+        this.bolt.flashCount++
+        this.generateCanvas()
+        this.renderCanvas()
+        await this.updateTiles()
+        if (this.bolt.flashCount < this.bolt.totalFlashes) {
+          this.bolt.delay = this.getStepMilliseconds()
+          this.bolt.state = 'midFlash'
+        } else {
+          this.bolt.delay = this.getStepMilliseconds()
+          this.bolt.state = 'postFlash'
+        }
+        break;
+      case 'midFlash':
+        await this.clearTiles()
+        this.bolt.delay = this.getStepMilliseconds()
+        this.bolt.state = 'flash'
+        break;
+      case 'postFlash':
+        const duration = 1000 + Math.floor(Math.random() * 1000)
+        await this.clearTiles(duration)
+        this.bolt.delay = duration + 1000 + Math.floor(Math.random() * 3000)
+        this.bolt.state = 'waiting'
+        break;
+      case 'waiting':
+        this.bolt = this.createBolt()
+        break;
+    }
+  }
+
+  generateCanvas() {
+    const canvas = [...Array(this.bounds.height)].map(() =>
+      [...Array(this.bounds.width)]
+    )
+    let x = this.bolt.origin
+    for (let y = 0; y < this.bounds.height; y++) {
+      x = x - 1 + Math.floor(Math.random() * 3)
+      x = Math.max(0, Math.min(this.bounds.width - 1, x))
+      canvas[y][x] = '⚡️'
+    }
+    this.canvas = canvas
   }
 
   renderCanvas() {
     const colors = Array(this.bounds.width * this.bounds.height).fill(noColor)
     this.canvas.forEach((row, y) => {
       this.canvas[y].forEach((col, x) => {
-        if (col === undefined) return
+        if (col !== '⚡️') return
         let index = (Math.round(y) * this.bounds.width) + x
-        colors[index] = parseColor(col)
+        colors[index] = {
+          saturation: 0,
+          brightness: 1,
+          kelvin: Math.random() < 0.25 ? 6000 : 9000
+        }
       })
     })
     this.colors = colors
+  }
+
+  async clearTiles(duration = 0) {
+    await this.device.tileSetTileState64({
+      tile_index: this.tiles[0].tile_index,
+      length: this.tiles.length,
+      duration,
+      colors: Array(64).fill(noColor)
+    }).catch(console.error)
   }
 
   async updateTiles() {
@@ -93,7 +145,7 @@ module.exports = class {
   }
 
   getStepMilliseconds() {
-    return 1000
+    return 200
   }
 
 }
